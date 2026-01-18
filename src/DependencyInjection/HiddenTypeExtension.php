f5b8b5f885db8235c8d82c1a444c74705f988df2
Nur Hidayatullah <kematjaya0@gmail.com>
2026-01-18 08:12:28 +0700

    upgrade sf 8

--- src/DependencyInjection/HiddenTypeExtension.php
+++ src/DependencyInjection/HiddenTypeExtension.php
@@ -13,7 +13,7 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
 class HiddenTypeExtension extends Extension 
 {
     
-    public function load(array $configs, ContainerBuilder $container) 
+    public function load(array $configs, ContainerBuilder $container) :void
     {
         $locator = new FileLocator(__DIR__.'/../Resources/config');
         $loader = new YamlFileLoader($container, $locator);
